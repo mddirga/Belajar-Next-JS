1c18301e678529c71e4c4b5e0a3835571e3feb13
mddirga <madedirga22@gmail.com>
2024-07-28 23:08:12 +0800

    Pages & Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "@/components/layouts/Navbar";
 
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <div>Hello Next</div>
+    </div>
+  );
 }
